561765d11ce7fabaf928e8227ce677ab27e6ba74
yanghaojia <2453883990@qq.com>
2020-01-11 19:46:02 +0800

    Securing the Eureka server, SpringBoot Admin server and Application client

--- eureka-server/src/main/java/com/sz/springcloudsamples/eureka/config/security/EurekaServerWebSecurityConfig.java
+++ eureka-server/src/main/java/com/sz/springcloudsamples/eureka/config/security/EurekaServerWebSecurityConfig.java
@@ -1,19 +1,19 @@
-package com.sz.springcloudsamples.common.config.security;
+package com.sz.springcloudsamples.eureka.config.security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author Yanghj
- * @date 1/3/2020
+ * @date 1/11/2020
  */
-@Configuration
-public class DefaultWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class EurekaServerWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
-                .ignoringAntMatchers("/actuator/**");
+        http.csrf().ignoringAntMatchers("/eureka/**");
         super.configure(http);
     }
 }
--- spring-boot-admin-server/src/main/java/com/sz/springcloudsamples/admin/SpringBootAdminServerApplication.java
+++ spring-boot-admin-server/src/main/java/com/sz/springcloudsamples/admin/SpringBootAdminServerApplication.java
@@ -1,19 +1,22 @@
 package com.sz.springcloudsamples.admin;
 
+import com.sz.springcloudsamples.admin.config.security.AdminServerWebSecurityConfig;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
 @EnableEurekaClient
+@Import({AdminServerWebSecurityConfig.class})
 public class SpringBootAdminServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
 
 }
